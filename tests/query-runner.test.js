482e51342044100698c8e9c59fe3a204db955146
Paulo Cesar <metasoft.brasil@gmail.com>
2020-05-16 05:52:01 -0300

    improve spec

--- tests/query-runner.test.js
+++ tests/query-runner.test.js
@@ -1,27 +1,68 @@
 const assert = require('assert');
 const QueryRunner = require('../src/query-runner');
 
+const sql = `
+-->
+-- example of empty command: { "db": "default", "output": "vizualize" }
+DECLARE @temp TABLE (id int, value varchar(20))
+
+INSERT INTO @temp(id, value)
+VALUES (1, 'First'), (2, 'Second')
+
+SELECT * FROM @temp
+
+--> { "db": "anotherLabel", "output": "markdown", "title": "try Markdown" }
+
+SELECT 'First' col1, 'Second' col2
+
+--> { "db": "anotherLabel", "output": "csv", "title": "try CSV" }
+
+SELECT 'First' col1, 'Second' col2
+
+--> { "db": "anotherLabel", "output": "json", "title": "try JSON" }
+
+SELECT 'First' col1, 'Second' col2
+`;
+
+const testResult = `
+=== QUERY RESULT ===
+
+ # id value
+-- -- -------
+#0 1  First
+#1 2  Second
+
+
+### try Markdown
+
+col1 | col2
+:-- | :--
+First | Second
+
+
+=== try CSV ===
+
+"col1","col2"
+"First","Second"
+
+
+/* === try JSON === */
+[
+    {
+        "col1": "First",
+        "col2": "Second"
+    }
+]
+`.trim();
+
 describe('QueryRunner', function() {
     it('should format a result', async function() {
-        const nvimLines = [
-            'select 1',
-            '-->',
-            'select Id, DmsId, Vin, Make, Model, Year',
-            'from DmsVehicle where DmsCustomerId = 31726310',
-            '--> ',
-            'select top 5 zDealerID, zMenuID, zVIN, zCustomerLastName',
-            'from tblMenu where zDealerID = 1750',
-            '',
-            '--> {"title": "Try JSON", "output": "json"}',
-            'select top 1 zCustomerFirstName, zCustomerLastName',
-            'from tblMenu where zDealerID = 1750',
-            '',
-            '-->',
-            'select top 1 from Invalid',
-        ];
-        const qr = new QueryRunner(nvimLines, 40);
+        const qr = new QueryRunner(sql.split('\n'), 40);
         const r = await qr.results();
-        console.log(r);
-        assert(r.length > 0, 'should return a result');
+
+        assert.equal(
+            r.map((l) => l.trim()).join('\n').trim(),
+            testResult.split('\n').map((l) => l.trim()).join('\n').trim()
+        );
     });
 });
